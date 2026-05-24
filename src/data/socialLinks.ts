ca373c3d7d57cbdd4a250d707c3321c2df5e9593
wc-yuhuai <1449776576@qq.com>
2026-05-25 02:49:15 +0800

    feat(SocialLinks): 添加多个社交平台图标支持
    新增 Bilibili、QQ、微信、网易云音乐等社交平台的图标组件，
    并更新社交链接数据配置以支持这些新的社交平台连接。

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -5,5 +5,20 @@ export const socialLinks: SocialLink[] = [
     name: 'GitHub',
     url: 'https://github.com',
     color: 'hover:bg-gray-900 hover:text-white dark:hover:bg-white dark:hover:text-gray-900'
+  },
+  {
+    name: 'QQ',
+    url: 'tencent://message/?uin=YOUR_QQ_NUMBER',
+    color: 'hover:bg-blue-500 hover:text-white'
+  },
+  {
+    name: 'WeChat',
+    url: '#wechat',
+    color: 'hover:bg-green-500 hover:text-white'
+  },
+  {
+    name: 'Bilibili',
+    url: 'https://space.bilibili.com/',
+    color: 'hover:bg-pink-500 hover:text-white'
   }
 ]
