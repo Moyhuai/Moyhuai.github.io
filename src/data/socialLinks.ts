d12cd808a254854515006e6a5d7d7f536f0d368d
wc-yuhuai <1449776576@qq.com>
2026-05-26 00:40:19 +0800

    fix(data): 更新社交媒体链接配置
    - 将GitHub链接更新为个人仓库地址
    - 将QQ链接从本地协议改为在线加群链接

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -3,12 +3,12 @@ import type { SocialLink } from '../types'
 export const socialLinks: SocialLink[] = [
   {
     name: 'GitHub',
-    url: 'https://github.com',
+    url: 'https://github.com/moyhuai',
     color: 'hover:bg-gray-900 hover:text-white dark:hover:bg-white dark:hover:text-gray-900'
   },
   {
     name: 'QQ',
-    url: 'tencent://message/?uin=YOUR_QQ_NUMBER',
+    url: 'https://qm.qq.com/q/ZncJVD2d6q',
     color: 'hover:bg-blue-500 hover:text-white'
   },
   {
